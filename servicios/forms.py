71b1b2fd9d6a67cf07145612dd5d8b3f8137ad21
lagomoura <mouralago@live.com>
2023-12-26 00:40:09 -0300

    New_service form done

--- servicios/forms.py
+++ servicios/forms.py
@@ -12,5 +12,5 @@ class FormServicio(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control'})
+            self.fields[field].widget.attrs.update({'class': 'form-control'}) #. Agregamos una clase a cada field.
             
