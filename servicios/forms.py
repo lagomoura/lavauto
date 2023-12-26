f68a4e4e30ebe7b7fcdcabb13c98f3e62d3cb682
lagomoura <mouralago@live.com>
2023-12-26 00:41:18 -0300

    NewServices Form done

--- servicios/forms.py
+++ servicios/forms.py
@@ -8,9 +8,15 @@ class FormServicio(ModelForm):
         # . Va a traer todos los campos de Servicio, menos los mencionados
         exclude = ['finalizado', 'protocolo_servicio']
 
-    #. Manera de sobreescribir una clase.
+    # . Manera de sobreescribir una clase.
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control'}) #. Agregamos una clase a cada field.
-            
+            # . Agregamos una clase a cada field.
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+        choices = list()
+        for i, j in self.fields["categoria_mantenimiento"].choices:
+            categoria = CategoriaMantenimiento.objects.get(titulo=j)
+            choices.append((i, categoria.get_titulo_display()))
+        self.fields['categoria_mantenimiento'].choices = choices
--- servicios/views.py
+++ servicios/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .forms import FormServicio
+from django.http import HttpResponse
 
 
 def nuevo_servicio(request):
-    form = FormServicio()
-    return render(request, 'nuevo_servicio.html', {'form': form})
+    if request.method == "GET":
+        form = FormServicio()
+        return render(request, 'nuevo_servicio.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServicio(request.POST)
+
+        if form.is_valid():  # . El is_valid ya valida automaticamente el formulario con base en lo seteado en el archivo de form
+            form.save()
+            return HttpResponse("Guardado con suceso!!")
+
+        else:
+            return render(request, 'nuevo_servicio', {'form': form})
